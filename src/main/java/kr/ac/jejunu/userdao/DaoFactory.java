2e86b092df484dda26ceddceb01ae3b2ce60f052
C7C4FF <80257041+C7C4FF@users.noreply.github.com>
2022-04-22 16:51:53 +0900

    05. 각자가 만든 DI를 위한 Factory를 Spring으로 전환하여보세요

--- src/main/java/kr/ac/jejunu/userdao/DaoFactory.java
+++ src/main/java/kr/ac/jejunu/userdao/DaoFactory.java
@@ -1,12 +1,18 @@
 package kr.ac.jejunu.userdao;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class DaoFactory {
-    public UserDao getUserDao(){
-        return new UserDao(getConnectionMaker());
+    @Bean
+    public UserDao userDao(){
+        return new UserDao(connectionMaker());
 
     }
-
-    public ConnectionMaker getConnectionMaker() {
+    @Bean
+    public ConnectionMaker connectionMaker() {
         return new JejuConnectionMaker();
     }
 }
