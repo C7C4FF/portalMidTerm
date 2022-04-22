bd0b09a0727982cb2e9af9e19e2d3081ce941ac4
C7C4FF <80257041+C7C4FF@users.noreply.github.com>
2022-04-22 16:21:07 +0900

    01 사용자 추가기능을 만들어주세요.

---
+++ src/main/java/kr/ac/jejunu/userdao/UserDao.java
@@ -0,0 +1,53 @@
+package kr.ac.jejunu.userdao;
+
+import java.sql.*;
+
+public class UserDao {
+    public User get(Integer id) throws ClassNotFoundException, SQLException {
+        Connection connection = getConnection();
+
+        PreparedStatement preparedStatement =
+                connection.prepareStatement("select * from userinfo where id = ?");
+        preparedStatement.setInt(1, id);
+
+        ResultSet resultSet = preparedStatement.executeQuery();
+        resultSet.next();
+
+        User user = new User();
+        user.setId(resultSet.getInt("id"));
+        user.setName(resultSet.getString("name"));
+        user.setPassword(resultSet.getString("password"));
+
+        resultSet.close();
+        preparedStatement.close();
+        connection.close();
+        return user;
+    }
+
+    public void insert(User user) throws SQLException, ClassNotFoundException {
+        Connection connection = getConnection();
+
+        PreparedStatement preparedStatement =
+                connection.prepareStatement("insert into userinfo(name, password) values (?, ?)", Statement.RETURN_GENERATED_KEYS);
+        preparedStatement.setString(1, user.getName());
+        preparedStatement.setString(2, user.getPassword());
+        preparedStatement.executeUpdate();
+        ResultSet resultSet = preparedStatement.getGeneratedKeys();
+        resultSet.next();
+
+        user.setId(resultSet.getInt(1));
+
+        resultSet.close();
+        preparedStatement.close();
+        connection.close();
+    }
+
+
+    public Connection getConnection() throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection connection =
+                DriverManager.getConnection("jdbc:mysql://localhost/jeju"
+                        , "root", "1234");
+        return connection;
+    }
+}
